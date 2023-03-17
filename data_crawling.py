3b1bb7da20175efaa4140fda7f1b3a4c4f1bef8c
sjc4197 <63084925+sjc4197@users.noreply.github.com>
2023-03-17 23:55:37 +0900

    예외처리
    예외처리

--- data_crawling.py
+++ data_crawling.py
@@ -1,7 +1,8 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
-for i in range(1, 5):
+for i in range(1, 1000):
     url = "https://distiller.com/search?page="+str(i)+"&sort=total_num_of_ratings"
     res = requests.get(url)
     res.raise_for_status()
@@ -16,9 +17,17 @@ for i in range(1, 5):
 
         name = names[idx].text.strip()  # 현재 이미지에 해당하는 이름 정보 추출
         filename = "{}.jpg".format(name.replace("/", "-"))  # 파일 이름 생성 (슬래시는 대시로 변경)
+        filepath = str((i-1)*10+idx+1)+"_"+filename
 
-        image_res = requests.get(image_url)
-        image_res.raise_for_status()
+        try:
+            image_res = requests.get(image_url)
+            image_res.raise_for_status()
 
-        with open(str((i-1)*10+idx+1)+"_"+filename, "wb") as f:
-            f.write(image_res.content)
+            with open(filepath, "wb") as f:
+                f.write(image_res.content)
+
+        except OSError as e:
+            if e.errno == 22:  # 파일 이름에 사용할 수 없는 문자가 포함된 경우
+                print(f"Skipping {filepath} due to invalid characters in filename.")
+            else:
+                print(f"Error occurred while downloading {filepath}: {e}")
