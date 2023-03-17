1bf662a201fb8aace71bd3485aee1c8106fcd5de
sjc4197 <63084925+sjc4197@users.noreply.github.com>
2023-03-17 22:27:35 +0900

    최종확정 크롤링코드
    굳

--- data_crawling.py
+++ data_crawling.py
@@ -1,23 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-for i in range(1,3):
+for i in range(1, 5):
     url = "https://distiller.com/search?page="+str(i)+"&sort=total_num_of_ratings"
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text,"lxml")
 
-    images = soup.find_all("div",attrs={"class":"image"})
-
+    images = soup.find_all("div", attrs={"class": "image"})
+    names = soup.find_all("div", attrs={"class": "name"})
     for idx, image in enumerate(images):
-    #print(image["style"])
         image_url = image['style'].split('url(')[1].split(')')[0]
         if image_url.startswith("//"):
             image_url = "https:"  + image_url
 
-        print(image_url)
+        name = names[idx].text.strip()  # 현재 이미지에 해당하는 이름 정보 추출
+        filename = "{}.jpg".format(name.replace("/", "-"))  # 파일 이름 생성 (슬래시는 대시로 변경)
+
         image_res = requests.get(image_url)
         image_res.raise_for_status()
 
-        with open("whiskey{}.jpg".format((i-1)*10+idx+1),"wb") as f:
+        with open(str((i-1)*10+idx+1)+"_"+filename, "wb") as f:
             f.write(image_res.content)
