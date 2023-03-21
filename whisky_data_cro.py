1cb0fa62c0618ae2e0066259e808bcf2a5086e22
sjc4197 <63084925+sjc4197@users.noreply.github.com>
2023-03-22 01:19:52 +0900

    Update whisky_data_cro.py

--- whisky_data_cro.py
+++ whisky_data_cro.py
@@ -10,8 +10,7 @@ with open("whiskey_info.csv", "w", newline="") as f:
     writer.writerow(["Name","kind_of_whisky" ,"Country", "Region" , "age", "abv", "Cask type", "Style", "Taste Note"])
 
     # 페이지 수 조절
-    # page_count = 2401
-    page_count = 1 #임시
+    page_count = 2
 
     for page in range(1, page_count + 1):
         # 페이지 URL 생성
@@ -36,15 +35,50 @@ with open("whiskey_info.csv", "w", newline="") as f:
             split_data = basic_data.split("\n")
             split_data2 = split_data[2].split("//")
 
-            name = split_data2[0].replace(" " , "") #이름
-            kind_of_whisky = split_data[0].replace(" " , "") #위스키 종류
-            country = split_data2[1].replace(" " , "").split(",")[1] #만들어진 나라
-            region = split_data2[1].replace(" " , "").split(",")[0] #만들어진 지역
-            age = soup_whiskey.find("li", class_="age").text.strip().split("\n")[1] #숙성연수
-            abv = soup_whiskey.find("li", class_="abv").text.strip().split("\n")[1] #알코올 도수
-            cask = soup_whiskey.find("li", class_="cask-type").text.strip().split("\n")[1] #캐스크 타입
-            style = soup_whiskey.find("li", class_="whiskey-style").text.strip().split("\n")[1] #위스키 스타일
-            taste = soup_whiskey.find('blockquote').text.strip() #위스키 테이스팅 노트
+            try:
+                name = split_data2[0].replace(" " , "") #이름
+            except:
+                name = "None"
+                
+            try:
+                kind_of_whisky = split_data[0].replace(" " , "") #위스키 종류
+            except:
+                kind_of_whisky = "None"
+                
+            try:
+                country = split_data2[1].replace(" " , "").split(",")[1] #만들어진 나라
+            except:
+                country = "None"
+                
+            try:
+                region = split_data2[1].replace(" " , "").split(",")[0] #만들어진 지역
+            except:
+                region = "None"
+                
+            try:
+                age = soup_whiskey.find("li", class_="age").text.strip().split("\n")[1] #숙성연수
+            except:
+                age = "None"
+                
+            try:
+                abv = soup_whiskey.find("li", class_="abv").text.strip().split("\n")[1] #알코올 도수
+            except:
+                abv = "None"
+                
+            try:
+                cask = soup_whiskey.find("li", class_="cask-type").text.strip().split("\n")[1] #캐스크 타입
+            except:
+                cask = "None"
+                
+            try:
+                style = soup_whiskey.find("li", class_="whiskey-style").text.strip().split("\n")[1] #위스키 스타일
+            except:
+                style = "None"
+                
+            try:
+                taste = soup_whiskey.find('blockquote').text.strip() #위스키 테이스팅 노트
+            except:
+                taste = "None"
             cnt = cnt+1
             print(cnt)
 
